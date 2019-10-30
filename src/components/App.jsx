52b880d3283f07f06f8b544ea1aebee8b7bec599
Preston Lund <prestonlund@gmail.com>
2019-10-29 20:26:47 -0600

    add delete reminders function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,17 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: ''
+            text: '',
+            dueDate: ''
         }
     }
 
     addReminder() {
-        this.props.addReminder(this.state.text);
+        this.props.addReminder(this.state.text, this.state.dueDate);
     }
 
     deleteReminder(id) {
-        console.log('deleting in application', id);
-        console.log('this.props', this.props);
+        this.props.deleteReminder(id);
     }
 
     renderReminders() {
@@ -55,6 +55,11 @@ class App extends Component {
                             className="form-control"
                             placeholder="I have to..."
                             onChange={event => this.setState({text: event.target.value})}
+                        />
+                        <input
+                            className="form-control"
+                            type="datetime-local"
+                            onChange={event => this.setState({dueDate: event.target.value})}
                         />
                     </div>
                     <button 
