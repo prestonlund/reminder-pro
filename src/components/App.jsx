eea2641b03870071ef118c10a159cda1193579b7
Preston Lund <prestonlund@gmail.com>
2019-11-01 13:08:33 -0600

    add clear reminders button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { connect } from 'react-redux';
-import { addReminder, deleteReminder } from '../actions';
+import { addReminder, deleteReminder, clearReminders } from '../actions';
 import moment from 'moment';
 
 class App extends Component {
@@ -75,6 +75,12 @@ class App extends Component {
                     </button>
                 </div>
                 { this.renderReminders() }
+                <div
+                    className="btn btn-danger"
+                    onClick={() => this.props.clearReminders()}
+                >
+                    Clear Reminders
+                </div>
             </div>
         )
     }
@@ -86,4 +92,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
